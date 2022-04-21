2b72b586d1c6529b07d8b44d59805c69af298da2
Sean McGinty <31000120+s3ansh33p@users.noreply.github.com>
2022-04-22 02:20:05 +0800

    Experiments with other data types

--- utils.js
+++ utils.js
@@ -145,5 +145,80 @@ function updateChecksum(bytes, checksum) {
     return checksum;
 }
 
-const filename = "c-converted.xcp"
-generateHex("Max 30720\nTest", "Sean", "McGinty")
+// const filename = "c-converted.xcp"
+// const filename = "a-(Mem).xcp"
+// const filename = "b-(String).xcp"
+const filename = "y-(Expression).xcp"
+// const filename = "d-(Program).xcp"
+// generateHex("Max 30720\nTest", "Sean", "McGinty")
+
+async function readLocalFile() {
+    fs.readFile(filename, 'utf8', (err, data) => {
+        if (err) throw err;
+        filedata = new Uint8Array(data.length);
+        for (let i = 0; i < data.length; i++) {
+            filedata[i] = data.charCodeAt(i);
+        }
+        // console.log(filedata)
+
+        let folname_len = parseInt(hexToAscii(filedata[19].toString(16)));
+        let folname = '';
+        for (let i=0; i<folname_len - 1; i++) {
+            folname += hexToAscii(filedata[20+i].toString(16));
+        }
+        let varname_len = parseInt(hexToAscii(filedata[21+folname_len].toString(16)));
+        let varname = '';
+        for (let i=0; i<varname_len - 1; i++) {
+            varname += hexToAscii(filedata[22+folname_len+i].toString(16));
+        }
+        // metadata
+        let meta = ''
+        let min = 73;
+        for (let i=min; i<min+3; i++) { // haven't found the edge case for 4 bytes
+            meta += hexToAscii(filedata[i].toString(16));
+        }
+        meta = asciiToHex(meta).toUpperCase();
+        console.log(meta)
+        const lookup = [
+            {
+                'byte': '47',
+                'info': "Program/Text",
+            },
+            {
+                'byte': '4E',
+                'info': "Memory",
+            },
+            {
+                'byte': '05',
+                'info': "String",
+            },
+            {
+                'byte': '12',
+                'info': "Expression",
+            }
+        ];
+        // read first metadata byte and log info
+        let metatype = lookup.find(x => x.byte == (meta[0]+""+meta[1]).toString(16)).info;
+
+        // check if unlocked
+        let unlocked = ((meta[2]+""+meta[3]).toString(16) == 0x55) ? true : false;
+
+        let text = '';
+        let index = 100+folname_len+varname_len;
+        // check if string or expression as the have different index
+        if (metatype == "String" || metatype == "Expression") {
+            index = 100-15+folname_len+varname_len; // will need to confirm - will also need to look into expressions at some point
+        }
+        let raw = '';
+        while (filedata[index] != 0x00) {
+            text += hexToAscii(filedata[index].toString(16));
+            raw += filedata[index].toString(16);
+            index++;
+        }
+        console.log(`Reading file: ${filename} (${unlocked ? "Unlocked" : "Locked"}) | Type: ${metatype} | Varname: ${varname} | Folder: ${folname}`); 
+        console.log(text)
+        console.log(raw)
+    });
+}
+
+readLocalFile()
